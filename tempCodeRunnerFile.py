54ee801cd7680ec82de1274b34962ae77acda819
smit kunpara <smitkunpara@gmail.com>
2023-09-20 11:36:45 +0530

    added gitignore

--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-encrypted_message = public_key.encrypt(
-                message,
-                padding.OAEP(
-                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
-                    algorithm=hashes.SHA256(),
-                    label=None
-                )
-            )
