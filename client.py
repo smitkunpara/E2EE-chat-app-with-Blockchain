3a712fb471b44e803b189563b5c841953d0a6d68
smit kunpara <smitkunpara@gmail.com>
2023-11-20 15:12:41 +0530

    restuctured the code

--- client.py
+++ client.py
@@ -4,145 +4,127 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
 from cryptography.hazmat.primitives.asymmetric import rsa, padding
 import time
-import base64
-#################################################################
-def receive_messages():
-    print("receive_messages")
+import json,mysql.connector
+import ssl
+
+
+def reliable_send(ssl_client_socket,message):
+    message = json.dumps(message)
+    ssl_client_socket.send(message.encode('utf-8'))
+
+def reliable_recv(ssl_client_socket):
+    json_data = ""
     while True:
-        # print(not stop_event.is_set())
-        # print("working")
         try:
-            client_socket.settimeout(0.1)
-            message = client_socket.recv(1024).decode('utf-8')
-            client_socket.settimeout(None)
-            message = decrypt_message(pvt_key_obj, message)
-            print(message)
-        except Exception as e:
-            client_socket.settimeout(None)
-            if stop_event.is_set():
-                break
-            # print(f"Error: {e}")
-    print("receive_messages stopped")
-def send_message():
-    while True:
-        recipient = input("username: ")
-        stop_event.set()
-        message = "request_public_key:"+recipient
-        global receive_thread
-        time.sleep(0.3)
-        client_socket.send(message.encode('utf-8'))
-        while True:
-            try:
-                pub_key=client_socket.recv(4096).decode('utf-8')
-                break
-            except Exception as e:
-                print(f"Error: {e}")
-                continue
-        
-        stop_event.clear()
-        receive_thread = threading.Thread(target=receive_messages)
-        receive_thread.start()
-        if pub_key == "no_public_key":
-            print(pub_key)
-        elif pub_key == "user is offline":
-            print(pub_key)
-        else:
-            print(pub_key)
-            pub_key_obj = serialization.load_pem_public_key(
-                pub_key.encode('utf-8'),
-                backend=default_backend()
-            )
-            message = input("message: ")
-            encrypted_message = encrypt_message(pub_key_obj, message)
-            message_to_send = f"send:{recipient}:{base64.b64encode(encrypted_message).decode('utf-8')}"
-            client_socket.send(message_to_send.encode('utf-8'))
+            json_data += ssl_client_socket.recv(1024).decode('utf-8')
+            return json.loads(json_data)
+        except ValueError:
+            continue
 
+def register_secondary_device():
+    pass
 
-def encrypt_message(public_key, message):
-    message=message.encode('utf-8')
-    encrypted_message = public_key.encrypt(
-        message,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
+def generate_private_key():
+    private_key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
     )
-    return encrypted_message
+    return private_key
 
-def decrypt_message(private_key, encrypted_message):
-    encrypted_message = base64.b64decode(encrypted_message)
-    decrypted_message = private_key.decrypt(
-        encrypted_message,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    return decrypted_message
-
-def check_pvt_key():
-    file = open("private_key.txt", "+a")
-    f_pvt_key = file.read()
-    pvt_key = ""
-    if len(f_pvt_key) < 1650:
-        client_socket.send("no_pvt_key".encode('utf-8'))
-        pvt_key = client_socket.recv(4096).decode('utf-8')
-        file = open("private_key.txt", "w")
-        file.write(pvt_key)
-        file.close()
-    else:
-        client_socket.send("pvt_key".encode('utf-8'))
-        pvt_key = file.read()
+def load_pvt_key():
+    global my_username,my_password
     global pvt_key_obj
-    print(pvt_key)
-    try:
+    with open("pvt_key.pem", "a+") as f:
+        pass
+    with open("pvt_key.pem", "r") as f:
+        pvt_key_pem = f.read()
+    if pvt_key_pem == "" or pvt_key_pem == None:
+        pvt_key_obj = generate_private_key()
+        pvt_key_pem = pvt_key_obj.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.BestAvailableEncryption((my_username+my_password).encode('utf-8'))
+        ).decode('utf-8')
+        with open("pvt_key.pem", "w") as f:
+            f.write(pvt_key_pem)
+        public_key = pvt_key_obj.public_key()
+        public_key_pem = public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        ).decode('utf-8')
+        reliable_send(ssl_client_socket, ["update_public_key", my_username,public_key_pem])
+        if reliable_recv(ssl_client_socket) != "public key updated":
+            load_pvt_key()
+    else:
         pvt_key_obj = serialization.load_pem_private_key(
-            pvt_key.encode('utf-8'),
-            password=None,
+            pvt_key_pem.encode('utf-8'),
+            password=(my_username+my_password).encode('utf-8'),
             backend=default_backend()
         )
-    except Exception as e:
-        print(f"Error loading private key: {e}")
-        
-    file.close()
+
+def send_message():
+    print("type exit to exit")
+    while True:
+        message = input("Enter receiver username:")
+        if message == "exit":
+            global flag
+            flag.set()
+            break
+        else:
+            message = ["request_public_key", message[0]]
+            reliable_send(ssl_client_socket,message)
+        time.sleep(0.4)
 
 def login():
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
-    message_to_send = f"login:{username}:{password}"
-    client_socket.send(message_to_send.encode('utf-8'))
-    response = client_socket.recv(1024).decode('utf-8')
+    username = input("username: ")
+    password = input("password: ")
+    message = ["login", username, password]
+    reliable_send(ssl_client_socket,message)
+    response = reliable_recv(ssl_client_socket)
     if response == "login successful":
-        check_pvt_key()
-        print("Login successful")
+        global my_username
+        global my_password
+        my_username = username
+        my_password = password
+        load_pvt_key()
         receive_thread.start()
-        
+        print("login successful")
         send_message()
     else:
         print(response)
         menu()
-        
-        
-
+     
 def register():
-    username = input("Enter your username: ")
-    password = input("Enter your password: ")
-    # convert password to hash
-    password = password
-    message_to_send = f"register:{username}:{password}"
-    client_socket.send(message_to_send.encode('utf-8'))
-    response = client_socket.recv(1024).decode('utf-8')
+    username = input("username: ")
+    password = input("password: ")
+    message = ["register", username, password]
+    reliable_send(ssl_client_socket,message)
+    response = reliable_recv(ssl_client_socket)
     if response == "register successful":
-        check_pvt_key()
-        print("Register successful")
-        print("Login to continue")
+        print("login to continue")
         login()
     else:
         print(response)
         menu()
 
+def receive_messages():
+    while threading.Event().is_set() == False:
+        try:
+            message = reliable_recv(ssl_client_socket)
+            message = pvt_key_obj.decrypt(
+                message,
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                )
+            ).decode('utf-8')
+            print(message)
+        except Exception as e:
+            print(f"Error: {e}")
+            break
+    
 def menu():
     print("1. login")
     print("2. register")
@@ -156,18 +138,32 @@ def menu():
             register()
             break
         elif choice == "3":
-            client_socket.close()
+            ssl_client_socket.close()
             break
         else:
             print("Invalid choice. Try again.")
-###############################################################################
-###############################################################################
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(('localhost', 12345))
-pvt_key_obj=None
-receive_thread = threading.Thread(target=receive_messages)
-stop_event = threading.Event()
-flag=True
-print("Welcome to the chatroom!")
-menu()
 
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="UROP"
+)
+cursor = db.cursor()
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
+context.minimum_version = ssl.TLSVersion.TLSv1_2
+context.maximum_version = ssl.TLSVersion.TLSv1_3
+context.load_verify_locations('C:/Users/smitk/OneDrive/programs/python/projects/UROP/server-cert.pem')
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ssl_client_socket = context.wrap_socket(client_socket,server_hostname='smit')
+ssl_client_socket.connect(('localhost', 12345))
+pvt_key_obj = None
+receive_thread = threading.Thread(target=receive_messages)
+flag=threading.Event().clear()
+my_username = None
+second_device_pub_key = None
+my_password = None
+print("Connected to server")
+menu()
--- server.py
+++ server.py
@@ -1,116 +1,107 @@
 import socket
+import ssl
 import threading
 import mysql.connector
+import json
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-import random
-import string
+from cryptography.hazmat.primitives.asymmetric import padding,rsa
 import hashlib
 
+def reliable_send(ssl_client_socket,message):
+    message = json.dumps(message)
+    ssl_client_socket.send(message.encode('utf-8'))
 
-connected_clients = {}
-online_users = {}
-
-def genereate_random_string(length):
-    letters = string.ascii_lowercase
-    str=''.join(random.choice(letters) for i in range(length))
-    if str in connected_clients:
-        return genereate_random_string(length)
-    return str
-    
-
-def generate_key_pair(username):
-    private_key = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
-    public_key = private_key.public_key()
-    public_key_pem = public_key.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    public_key_pem_str = public_key_pem.decode('utf-8')
-    global db, cursor
-    queary=f"UPDATE users SET pub_key = '{public_key_pem_str}' WHERE username = '{username}'"
-    cursor.execute(queary)
-    db.commit()
-    return private_key
+def reliable_recv(ssl_client_socket):
+    json_data = ""
+    while True:
+        try:
+            json_data += ssl_client_socket.recv(1024).decode('utf-8')
+            return json.loads(json_data)
+        except ValueError:
+            continue
 
 def register(message):
     queary = f"SELECT * FROM users WHERE username = '{message[1]}'"
     cursor.execute(queary)
     rows = cursor.fetchall()
-    print("rows:",rows)
     if len(rows) == 0 or rows==None:
-
         queary = f"INSERT INTO users (username, password) VALUES ('{message[1]}', '{hashlib.md5(message[2].encode()).hexdigest()}')"
         cursor.execute(queary)
         db.commit()
-        client_socket.send("register successful".encode('utf-8'))
+        return "register successful"
     else:
-        client_socket.send("register failed".encode('utf-8'))
+        return "User already exists"
+
+def login(message):
+    queary = f"SELECT * FROM users WHERE username = '{message[1]}'"
+    cursor.execute(queary)
+    rows = cursor.fetchall()
+    if len(rows) == 0 or rows==None:
+        return "User not found"
+    else:
+        if rows[0][1] == hashlib.md5(message[2].encode()).hexdigest():
+            online_users[message[1]] = ssl_client_socket
+            global is_logged_in
+            is_logged_in = True
+            return "login successful"
+        else:
+            return "Incorrect password"
 
 def request_public_key(message):
-    print("message while requesting : ",message)
     if message[1] not in online_users:
-        client_socket.send("user is offline".encode('utf-8'))
+        return "user is offline"
     else:
         queary = f"SELECT * FROM users WHERE username = '{message[1]}'"
         cursor.execute(queary)
         rows = cursor.fetchall()
-        client_socket.send(rows[0][2].encode('utf-8'))
+        return rows[0][2]
 
-def send(message):
-    recipient_socket = online_users[message[1]]
-    recipient_socket.send(message[2].encode('utf-8'))
+def send_message(message, is_logged_in):
+    if is_logged_in==True:
+        if message[1] in online_users:
+            ssl_client_socket = online_users[message[1]]
+            message_to_send = f"send_message:{message[2]}"
+            reliable_send(ssl_client_socket, message_to_send)
+            return "Message sent"
+        else:
+            return "User is offline"
+    else:
+        return "Please login first"
 
+def logout(message, is_logged_in):
+    if is_logged_in==True:
+        online_users.pop(message[1])
+        return "logout successful"
+    else:
+        return "Please login first"
 
-def no_pvt_key(username):
-    private_key=generate_key_pair(username)
-    print("pvt key",private_key)
-    private_key_pem = private_key.private_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PrivateFormat.PKCS8,
-        encryption_algorithm=serialization.NoEncryption()
-    )
-    print("pem key",private_key_pem.decode('utf-8'))
-    private_key_pem_str = private_key_pem.decode('utf-8')
-    online_users[username].send(private_key_pem_str.encode('utf-8'))
+def update_public_key(message):
+    queary=f"UPDATE users SET pub_key = '{message[2]}' , update_time = NOW() WHERE username = '{message[1]}'"
+    cursor.execute(queary)
+    db.commit()
+    return "public key updated"
 
-def handle_client(client_socket, client_address,username):
-    try:
-        while True:
-            message = client_socket.recv(1024).decode('utf-8')
-            message=message.split(":")
-            print(message)
-            global cursor
-            global db
-            if message[0]=="login":
-                queary = f"SELECT * FROM users WHERE username = '{message[1]}' AND password = '{hashlib.md5(message[2].encode()).hexdigest()}'"
-                print(queary)
-                cursor.execute(queary)
-                rows = cursor.fetchall()
-                if len(rows) == 0:
-                    client_socket.send("login failed".encode('utf-8'))
-                else:
-                    username=message[1]
-                    online_users[username]=client_socket
-                    client_socket.send("login successful".encode('utf-8'))
-            elif message[0]=="register":
-                register(message)
-            elif message[0]=="request_public_key":
-                request_public_key(message)
-            elif message[0]=="send":
-                send(message)
-            elif message[0]=="no_pvt_key":
-                no_pvt_key(username)
-    except Exception as e:
-        print(f"Error: {e}")
-    finally:
-        online_users.pop(username)
-        client_socket.close()
+def handle_client(ssl_client_socket):
+    is_logged_in = False
+    while True:
+        message = reliable_recv(ssl_client_socket)
+        print(message)
+        if message[0] == "register":
+            message_result = register(message)
+        elif message[0] == "login":
+            message_result = login(message)
+        elif message[0] == "update_public_key":
+            message_result=update_public_key(message)
+        elif message[0] == "request_public_key":
+            message_result = request_public_key(message)
+        elif message[0] == "send_message":
+            message_result = send_message(message, is_logged_in)
+        elif message[0] == "logout":
+            message_result = logout(message, is_logged_in)
+        else:
+            print("Invalid message")
+        reliable_send(ssl_client_socket,message_result)
 
 db = mysql.connector.connect(
     host="localhost",
@@ -123,17 +114,24 @@ cursor = db.cursor()
 host = 'localhost'
 port = 12345
 
+
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context.minimum_version = ssl.TLSVersion.TLSv1_2
+context.maximum_version = ssl.TLSVersion.TLSv1_3
+context.load_cert_chain(certfile="C:/Users/smitk/OneDrive/programs/python/projects/UROP/server-cert.pem", keyfile="C:/Users/smitk/OneDrive/programs/python/projects/UROP/server-key.pem")
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
 server_socket.listen(5)
-
+online_users = {}
 print(f"Server listening on {host}:{port}")
 
 while True:
     client_socket, client_address = server_socket.accept()
-    newclient=genereate_random_string(10)
-    connected_clients[newclient]=client_socket
+    ssl_client_socket = context.wrap_socket(client_socket, server_side=True)
+    print(f"Connection from {client_address} has been established.")
+    client_thread = threading.Thread(target=handle_client, args=(ssl_client_socket,))
+    client_thread.start()
+    
 
-    client_handler = threading.Thread(target=handle_client, args=(client_socket, client_address,newclient))
-    client_handler.start()
-
+        
